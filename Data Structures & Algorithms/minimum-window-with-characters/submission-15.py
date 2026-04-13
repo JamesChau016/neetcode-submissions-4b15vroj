e52919ced77e2ab26f732b839f4938b2fd2d3b8b
Gia Huy (James) Chau <142286692+JamesChau016@users.noreply.github.com>
2026-04-13 03:48:53 -0400

    Add: minimum-window-with-characters - submission-15

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-15.py
@@ -0,0 +1,41 @@
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        ans=''
+        mlen=len(s)
+        
+        ds={}
+        dt={}
+        for i in t:
+            if dt.get(i):
+                dt[i]+=1
+            else:
+                dt[i]=1
+            
+        req=len(dt)
+        formed=0
+        l=0
+        bestl=0
+        bestr=0
+        for r in range(len(s)):
+            if ds.get(s[r]):
+                ds[s[r]]+=1
+            else:
+                ds[s[r]]=1
+            
+            if dt.get(s[r]):
+                if dt[s[r]]==ds[s[r]]:
+                    formed+=1
+
+            while formed==req:
+                if r-l+1<=mlen:
+                    mlen=r-l+1
+                    bestl=l
+                    bestr=r+1
+                ds[s[l]]-=1
+                if dt.get(s[l]):
+                    if dt[s[l]]>ds[s[l]]:
+                        formed-=1
+                l+=1
+        ans=s[bestl:bestr]
+
+        return ans
