4af070a52f0232ba4c08d0a02f0e9d78da7ce0f8
Gia Huy (James) Chau <142286692+JamesChau016@users.noreply.github.com>
2026-04-13 03:22:44 -0400

    Add: minimum-window-with-characters - submission-10

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-10.py
@@ -0,0 +1,37 @@
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        def check(ds: Dict, dt: Dict) -> bool:
+            for k,v in dt.items():
+                if ds.get(k):
+                    if ds.get(k) < v:
+                        return False
+                else:
+                    return False
+            return True
+        
+        ans=''
+        mlen=len(s)
+        
+        ds={}
+        dt={}
+        for i in t:
+            if dt.get(i):
+                dt[i]+=1
+            else:
+                dt[i]=1
+        
+        l=0
+        for r in range(len(s)):
+            if ds.get(s[r]):
+                ds[s[r]]+=1
+            else:
+                ds[s[r]]=1
+            
+            while check(ds,dt):
+                if r-l+1<=mlen:
+                    mlen=r-l+1
+                    ans=s[l:r+1]
+                ds[s[l]]-=1
+                l+=1
+
+        return ans
