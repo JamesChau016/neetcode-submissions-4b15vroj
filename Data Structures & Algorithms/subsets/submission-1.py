84b5699105ec69d86db9a37e3916711cf2be5c1d
Gia Huy (James) Chau <142286692+JamesChau016@users.noreply.github.com>
2026-06-14 09:42:10 +0700

    Add: subsets - submission-1

---
+++ Data Structures & Algorithms/subsets/submission-1.py
@@ -0,0 +1,22 @@
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        res=[]
+        def backtrack(subnet,index):
+            nonlocal res
+            
+            if index==len(nums):
+                res.append(subnet[:])
+                return
+            
+            subnet.append(nums[index])
+
+            backtrack(subnet, index+1)
+
+            subnet.pop()
+            
+            backtrack(subnet,index+1)
+
+        index=0
+        subnet=[]
+        backtrack(subnet,index)
+        return res
