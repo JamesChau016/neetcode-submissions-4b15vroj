1b0b30018553679828860db9a5a7ae19e20968ad
Gia Huy (James) Chau <142286692+JamesChau016@users.noreply.github.com>
2026-07-09 21:25:14 +0700

    Add: course-schedule - submission-5

---
+++ Data Structures & Algorithms/course-schedule/submission-5.py
@@ -0,0 +1,30 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        d={}
+        res=True
+
+        for i in prerequisites:
+            if not d.get(i[0]):
+                d[i[0]]=[i[1]]
+            else:
+                d[i[0]].append(i[1])
+
+        def dfs(course,visited):
+            nonlocal res
+            if not d.get(course):
+                return
+
+            visited.add(course)
+            for i in d.get(course):
+                if i in visited:
+                    res=False
+                    return
+                dfs(i,visited)
+            visited.remove(course)
+        
+        for i in range(numCourses):
+            dfs(i,set())
+            d[i]=[]
+
+        return res
+
