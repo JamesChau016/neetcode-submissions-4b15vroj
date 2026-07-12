851a88ce02682024a27a3d5bc68b7d22b4c4f8f2
Gia Huy (James) Chau <142286692+JamesChau016@users.noreply.github.com>
2026-07-12 17:58:48 +0700

    Add: valid-tree - submission-3

---
+++ Data Structures & Algorithms/valid-tree/submission-3.py
@@ -0,0 +1,35 @@
+class Solution:
+    def validTree(self, n: int, edges: List[List[int]]) -> bool:
+        res=True
+        d={}
+        visited=set()
+
+        for i in edges:
+            if not d.get(i[0]):
+                d[i[0]]=[i[1]]
+            else:
+                d[i[0]].append(i[1])
+            if not d.get(i[1]):
+                d[i[1]]=[i[0]]
+            else:
+                d[i[1]].append(i[0])
+        
+        print(d)
+        def dfs(node,parent):
+            nonlocal res
+            if node in visited:
+                res=False
+                return
+            
+            visited.add(node)
+            for i in d.get(node,[]):
+                if i==parent:
+                    continue
+                dfs(i,node)
+
+        dfs(0,-1)
+        if len(visited)!=n:
+            res=False
+        
+        return res
+            
